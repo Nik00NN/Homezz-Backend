a315bd03cb19f305fd289a86da2b779ae4a09eb5
Nik00NN <birziloiu.mihaialexandru@outlook.com>
2024-10-15 22:34:08 +0300

    configured Entity Framework and Serilog for logging

---
+++ Homezz.API.Data/HomezzDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Homezz.API.Data
+{
+    public class HomezzDbContext : DbContext
+    {
+        public HomezzDbContext(DbContextOptions options) : base(options)
+        {
+           
+        }
+    }
+}
--- Homezz.API/Program.cs
+++ Homezz.API/Program.cs
@@ -1,20 +1,33 @@
+using Homezz.API.Data;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+var connectionString = builder.Configuration.GetConnectionString("HomezzConnectionString");
+builder.Services.AddDbContext<HomezzDbContext>(options => options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowAll", p => p.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
+});
+
+builder.Host.UseSerilog((context, loggerConfiguration) => loggerConfiguration.WriteTo.Console().ReadFrom.Configuration(configuration: context.Configuration));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 
